ce6506bdca4764159c713135a8a24628b11f0c37
gabrielviticov <gabrielviticov.ctt@gmail.com>
2024-02-26 10:52:40 -0300

    model: create

---
+++ CorporeHealth_Clinic.Tests/UnitDoctorTest.cs
@@ -0,0 +1,17 @@
+﻿using CorporeHealth_Clinic.API.Domains.Entities.Models;
+using Xunit;
+
+namespace CorporeHealth_Clinic.Tests
+{
+    public class UnitDoctorTest
+    {
+        [Fact]
+        public void OnTesting() 
+        {
+            Person person = new()
+            {
+                FirstName = "Hugo"
+            };
+        }
+    }
+}
